07963c602312e183722acb26c84bc3752d542a46
tasnim29 <tasnimmahmudnjr@gmail.com>
2025-12-02 11:42:46 +0600

    api functionality

---
+++ app/components/SalesChart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SalesChart = () => {
+  return <div></div>;
+};
+
+export default SalesChart;
---
+++ app/components/SalesTable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SalesTable = () => {
+  return <div></div>;
+};
+
+export default SalesTable;
